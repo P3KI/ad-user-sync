1a41cb707a85350849626400dd92a13e0631896c
Philipp Claves <philipp@p3ki.com>
2024-10-22 15:48:25 +0200

    Support for exporting and importing users in configureable AD paths. Support for more exporting and importing users properties.

--- UserExport.ps1
+++ UserExport.ps1
@@ -1,47 +1,109 @@
 ﻿
 param(
-    [string]$TransferGroup = "Transfer",
-    [string]$OutputFile = "Users.json"
+    [string]$ConfigFile = "ExportConfig.json",
+    [string]$OutputFile = ""
 )
 
+$CONFIG = (Get-Content -Raw $ConfigFile | ConvertFrom-Json)
+
+$FilterGroupNames = $CONFIG.FilterGroupNames
+
+$SearchSubPaths = $CONFIG.SearchSubPaths
+if (($SearchSubPaths -eq $null) -or ($SearchSubPaths.Length -eq 0)) { SearchSubPaths = @("CN=Users") }
+
+$BasePath = $CONFIG.BasePath
+if (($BasePath -eq $null) -or ($BasePath -eq "")) { Write-Host('Error: Domain "BasePath" must be set in config'); exit($false) }
+
+if ($OutputFile -eq "") { $OutputFile = $CONFIG.OutputFile }
+if ($OutputFile -eq "") { $OutputFile = "Users.json" }
+
+
+
 class User {
+    [string]$SubPath
     [string]$UserName
     [string]$Name
+    [string]$DisplayName
     [boolean]$Enabled
     [string]$GivenName
     [string]$Surname
+    [string]$Mail
+    [string]$basfSIAMID
+    [string]$Country
+    [string]$City
+    [string]$Company
+    [string]$Department
     $AccountExpirationDate
     [string[]]$Groups
 
-    User([string]$UserName, [string]$Name, [boolean]$Enabled, [string]$GivenName, [string]$Surname, $AccountExpirationDate, [string[]]$Groups) {
+    User([string]$SubPath, [string]$UserName, [string]$Name, [string]$DisplayName, [boolean]$Enabled, [string]$GivenName, [string]$Surname, [string]$Mail, [string]$basfSIAMID, [string]$Country, [string]$City, [string]$Company, [string]$Department, $AccountExpirationDate, [string[]]$Groups) {
+        $this.SubPath = $SubPath
         $this.UserName = $UserName
         $this.Name = $Name
+        $this.DisplayName = $DisplayName
         $this.Surname = $Surname
         $this.GivenName = $GivenName
+        $this.Mail = $Mail
+        $this.basfSIAMID = $basfSIAMID
+        $this.Country = $Country
+        $this.City = $City
+        $this.Company = $Company
+        $this.Department = $Department
         $this.Enabled = $Enabled
         $this.AccountExpirationDate = $AccountExpirationDate
         $this.Groups = $Groups
     }
 }
 
-$Users = (Get-ADGroupMember $TransferGroup)
+
+#Collect all users in the specified AD paths
+$Users = @()
+$SubPathsMap = @{}
+foreach ($SearchSubPath in $CONFIG.SearchSubPaths){
+    $SearchPath = $SearchSubPath + "," + $BasePath
+    $PathUsers = Get-ADUser -SearchBase $SearchPath -Filter '*' # 'objectClass -eq "user"'
+    foreach ($User in $PathUsers){
+        $Users += $User
+        $SubPathsMap[$User.SID] = $SearchSubPath
+    }
+}
+
 
 
 $Output = @()
 
 foreach ($User in $Users) {
     $Groups = (Get-ADPrincipalGroupMembership $User.SID).SamAccountName
-    $ADUser = (Get-ADUser -Properties AccountExpirationDate $User.SID)
 
-    if ($ADUser.AccountExpirationDate -ne $null){
-        $ExpirationDate = ($ADUser.AccountExpirationDate | Get-Date -Format "o")
+    if ($FilterGroupNames -eq $null){
+        #No group name filter
+        $Include = $true
     } else {
-        $ExpirationDate = $null
+        #Filter my security group name: User must be a member of at least one of them.
+        $Include = $false
+        foreach ($Filter in $FilterGroupNames){
+            if ($Groups.Contains($Filter)){
+                $Include = $true
+                break
+            }
+        }
     }
+    if ($Include){
 
-    $UserObj = [User]::new($User.SamAccountName, $User.name, $ADUser.Enabled, $ADUser.GivenName, $ADUser.Surname, $ExpirationDate, $Groups)
+        $ADUser = (Get-ADUser -Properties DisplayName,AccountExpirationDate,Mail,Company,Department,Country,City $User.SID) #basfSIAMID
 
-    $Output += $UserObj
+        if ($ADUser.AccountExpirationDate -ne $null){
+            $ExpirationDate = ($ADUser.AccountExpirationDate | Get-Date -Format "o")
+        } else {
+            $ExpirationDate = $null
+        }
+
+        $Path = $SubPathsMap[$ADUser.SID]
+
+        $UserObj = [User]::new($Path, $User.SamAccountName, $User.Name, $ADUser.DisplayName, $ADUser.Enabled, $ADUser.GivenName, $ADUser.Surname, $ADUser.Mail, $ADUser.basfSIAMID, $ADUser.Country, $ADUser.City, $ADUser.Company, $ADUser.Department, $ExpirationDate, $Groups)
+
+        $Output += $UserObj
+   }
 }
 
 $OutputJson = $Output | ConvertTo-JSON
--- UserImport.ps1
+++ UserImport.ps1
@@ -3,7 +3,7 @@
     [string] $ConfigFile = "ImportConfig.json"
 )
 
-$BASE_PATH = "CN=P3KI Managed,DC=l2,DC=dev,DC=p3ki,DC=com"
+#$BASE_PATH = "CN=P3KI Managed,DC=l2,DC=dev,DC=p3ki,DC=com"
 
 function Get-ExpirationDate($User) {
     if (($User.AccountExpirationDate -eq $null) -or ($User.AccountExpirationDate -eq "")){
@@ -29,6 +29,18 @@ function Config-GetGroupMapping() {
     return $map
 }
 
+function Config-GetSubPathMapping() {
+    param($Config)
+
+    $map = @{}
+    $Config.SubPathMap.psobject.properties | Foreach { $map[$_.Name] = $_.Value }
+
+    #Add default mapping
+    if ($map["*"] -eq $null ) { $map["*"] = "CN=Users" }
+
+    return $map
+}
+
 function Users-Import() {
     param($InputFile)
     return (Get-Content -Raw $InputFile | ConvertFrom-Json)
@@ -40,16 +52,24 @@ function Create-User() {
 	
     $AccountParams = @{
         #AccountPassword       = ConvertTo-SecureString $passCT -AsPlainText -Force;
-        #Path                  = $BASE_PATH;
-        AccountExpirationDate = Get-ExpirationDate($User);
+        Path                  = (Map-Path $User.SubPath);
+        AccountExpirationDate = (Get-ExpirationDate $User);
         CannotChangePassword  = $false;
         ChangePasswordAtLogon = $false;
-        Name                  = $User.name;
+        Name                  = $User.Name;
+        DisplayName           = $User.DisplayName;
         SAMAccountName        = $User.UserName;
         Surname               = $User.Surname;
         GivenName             = $User.GivenName;
+        EMail                 = $User.Mail;
+        Country               = $User.Country;
+        City                  = $User.City;
+        Company               = $User.Company;
+        Department            = $User.Department;
         Enabled               = $false;
     }
+
+    Write-Host "Create User: " ($AccountParams | Format-Table | Out-String)
 
     $ADUser = New-ADUser -PassThru @AccountParams
 	if ($ADUser -ne $null){
@@ -63,14 +83,21 @@ function Update-User(){
     $ExpirationDate = Get-ExpirationDate($User)
 	$Enabled = $ADUser.Enabled -AND $User.Enabled #Users must be manuelly (re-)enabled.
 	
-    Write-Host "Update" $ADUser "->" ($User)
+    #Write-Host "Update" $ADUser "->" ($User)
 	
     #For Some properties you can only set non-empty values otherwise Set-ADUser will fail.
     #To set to an empty string, you have to use Set-ADUser -Clear
-	if ($User.name)      {Set-ADUser -Identity $ADUser.SID -DisplayName           $User.name}      else {Set-ADUser -Identity $ADUser.SID -Clear "DisplayName"}
-	if ($User.GivenName) {Set-ADUser -Identity $ADUser.SID -GivenName             $User.GivenName} else {Set-ADUser -Identity $ADUser.SID -Clear "GivenName"}
-	if ($User.Surname)   {Set-ADUser -Identity $ADUser.SID -Surname               $User.Surname}   else {Set-ADUser -Identity $ADUser.SID -Clear "sn"} #If you want to set the surname, you need to set the Surname property, if you want to clear it you need to clear the "sn" attribute. Fuck that shit.
+    #Can not update the "Name" Property
+    if ($User.DisplayName){Set-ADUser -Identity $ADUser.SID -DisplayName           $User.DisplayName} else {Set-ADUser -Identity $ADUser.SID -Clear "displayName"}
+	if ($User.GivenName)  {Set-ADUser -Identity $ADUser.SID -GivenName             $User.GivenName}   else {Set-ADUser -Identity $ADUser.SID -Clear "givenName"}
+	if ($User.Surname)    {Set-ADUser -Identity $ADUser.SID -Surname               $User.Surname}     else {Set-ADUser -Identity $ADUser.SID -Clear "sn"} #If you want to set the surname, you need to set the Surname property, if you want to clear it you need to clear the "sn" attribute. Fuck that shit.
+    if ($User.Mail)       {Set-ADUser -Identity $ADUser.SID -EMail                 $User.Mail}        else {Set-ADUser -Identity $ADUser.SID -Clear "Mail"} #If you want to set the email address, you need to set the EMail property, if you want to clear it you need to clear the "Mail" attribute. Fuck that shit.
+    if ($User.Country)    {Set-ADUser -Identity $ADUser.SID -Country               $User.Country}     else {Set-ADUser -Identity $ADUser.SID -Clear "c"} #If you want to set the country, you need to set the Country property, if you want to clear it you need to clear the "c" attribute. Fuck fuck that shit.
+    if ($User.City)       {Set-ADUser -Identity $ADUser.SID -City                  $User.City}        else {Set-ADUser -Identity $ADUser.SID -Clear "l"} #If you want to set the city, you need to set the city property, if you want to clear it you need to clear the "l" attribute. Fuck fuck fuck that shit.
+    if ($User.Company)    {Set-ADUser -Identity $ADUser.SID -Company               $User.Company}     else {Set-ADUser -Identity $ADUser.SID -Clear "company"}
+    if ($User.Department) {Set-ADUser -Identity $ADUser.SID -Department            $User.Department}  else {Set-ADUser -Identity $ADUser.SID -Clear "department"}
 	Set-ADUser -Identity $ADUser.SID -AccountExpirationDate $ExpirationDate #This one could actually be set to null directly. We don't currently use that, but may later.
+
     Set-ADUser -Identity $ADUser.SID -Enabled $Enabled
 	
 	Update-Groups $ADUser.SID $User.Groups
@@ -79,7 +106,10 @@ function Update-User(){
 function Map-Groups(){
     param($Groups)
 
-    return $Groups | % { $GROUP_MAP[$_]} | Where { $_ }
+    $Ret = $Groups | % { $GROUP_MAP[$_]}
+    $Ret += $GROUP_MAP["*"]
+
+    return $Ret | Where { $_ }
 }
 
 function Update-Groups() {
@@ -98,25 +128,41 @@ function Update-Groups() {
     Add-ADPrincipalGroupMembership -Identity $UserName -MemberOf ($GroupSet | Where { ($MANAGED_GROUPS).Contains($_) })
 }
 
+function Map-Path(){
+    param($SubPath)
+
+    $MappedPath = $SUB_PATH_MAP[$SubPath]
+
+    if (($MappedPath -eq $null) -or ($MappedPath -eq "")) { 
+        $MappedPath = $SUB_PATH_MAP["*"]
+    }
+
+    #Write-Host "Path Map" $SubPath "->" $MappedPath
+
+    return $MappedPath + "," + $BASE_PATH
+}
 
 Write-Host "Config File:" $ConfigFile
 Write-Host "Input File" $InputFile
 
 $CONFIG = (Get-Content -Raw $ConfigFile | ConvertFrom-Json)
+$BASE_PATH = $CONFIG.BasePath
 $MANAGED_GROUPS = Config-GetManagedGroups($CONFIG)
 $GROUP_MAP = Config-GetGroupMapping($CONFIG)
+$SUB_PATH_MAP = Config-GetSubPathMapping($CONFIG)
 
 $USERS = Users-Import $InputFile
 
 #Write-Host $MANAGED_GROUPS
 #Write-Host $GROUP_MAP
 
-
 foreach ($User in $USERS){
     
     $UserName = $User.UserName
     $ADUser = (Get-ADUser -Filter { SamAccountName -eq $UserName }) 
 	Write-Host "===== Username:" $UserName " ( Exists:" ($ADUser -ne $null)") ====="
+    #Write-Host ($User | Format-Table | Out-String)
+
 
     if ($ADUser -eq $null){
         Create-User $User
