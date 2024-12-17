35891d3c5f4b1bdfa000ef50cf903b90303f77ec
Philipp Claves <philipp@p3ki.com>
2024-12-17 12:24:45 +0000

    Major Refactor to code. Now runs from a single file: UserSync.py for easier packaging.

---
+++ UserSync.py
@@ -0,0 +1,35 @@
+import argparse
+import json
+import sys
+
+parser = argparse.ArgumentParser(
+                    prog='UserImport',
+                    description='Import/Export Windows ActiveDirectory user accounts')
+
+parser.add_argument('--config', '-c', default = "UserSync.cfg", help="Configuration file to use. See README.")
+group = parser.add_mutually_exclusive_group(required=True)
+group.add_argument('--export', '-e', '-o', action='store_true', help="Export Mode: File to write user account data to")
+group.add_argument('--import', '-i', action='store_true', help="Import Mode: File containing the users to import")
+parser.add_argument("user_file", nargs=1, help="User list file for import/export")
+
+args = parser.parse_args()
+
+try:
+    # Read config
+    with open(args.config) as cfg:
+        config = json.load(cfg)
+except FileNotFoundError:
+    print("Error: Config file '{}' not found.".format(args.config), file=sys.stderr)
+    exit(1)
+
+print("Args:", args)
+
+if args.export:
+    from src.UserExport import UserExporter
+    exporter = UserExporter(file=args.user_file[0], config=config)
+    exporter.run()
+
+elif vars(args)["import"]:
+    from src.UserImport import UserImporter
+    importer = UserImporter(file=args.user_file[0], config=config)
+    importer.run()
