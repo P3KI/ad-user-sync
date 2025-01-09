2797b9e03460db48f45b83598fdf7dcebf1109a9
Felix Hofmann <felix@hofmann.rocks>
2025-01-09 14:28:47 +0100

    moved arg_parser out of exec block

--- UserSync.py
+++ UserSync.py
@@ -4,41 +4,48 @@ import argparse
 import json
 import sys
 
+arg_parser = argparse.ArgumentParser(
+    prog="UserImport",
+    description="Import/Export Windows ActiveDirectory user accounts",
+)
+
+arg_parser.add_argument(
+    "--config",
+    "-c",
+    default="UserSync.cfg",
+    help="Configuration file to use. See README.",
+)
+
+arg_group = arg_parser.add_mutually_exclusive_group(required=True)
+
+arg_group.add_argument(
+    "--export",
+    "-e",
+    "-o",
+    action="store_true",
+    help="Export Mode: File to write user account data to",
+)
+arg_group.add_argument(
+    "--import",
+    "-i",
+    action="store_true",
+    help="Import Mode: File containing the users to import",
+)
+arg_group.add_argument(
+    "--resolve",
+    "-r",
+    action="store_true",
+    help="Import Mode: File containing the users to import",
+)
+arg_parser.add_argument(
+    "user_file",
+    nargs=1,
+    help="User list file for import/export",
+)
+
+
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(
-        prog="UserImport",
-        description="Import/Export Windows ActiveDirectory user accounts",
-    )
-
-    parser.add_argument(
-        "--config",
-        "-c",
-        default="UserSync.cfg",
-        help="Configuration file to use. See README.",
-    )
-    group = parser.add_mutually_exclusive_group(required=True)
-    group.add_argument(
-        "--export",
-        "-e",
-        "-o",
-        action="store_true",
-        help="Export Mode: File to write user account data to",
-    )
-    group.add_argument(
-        "--import",
-        "-i",
-        action="store_true",
-        help="Import Mode: File containing the users to import",
-    )
-    group.add_argument(
-        "--resolve",
-        "-r",
-        action="store_true",
-        help="Import Mode: File containing the users to import",
-    )
-    parser.add_argument("user_file", nargs=1, help="User list file for import/export")
-
-    args = parser.parse_args()
+    args = arg_parser.parse_args()
 
     try:
         # Read config
@@ -48,7 +55,7 @@ if __name__ == "__main__":
         print(f"Error: Config file '{args.config}' not found.", file=sys.stderr)
         exit(1)
 
-    print("Args:", args)
+    print(f"Args: {args}")
 
     if args.export:
         from src.UserExport import UserExporter
@@ -59,5 +66,5 @@ if __name__ == "__main__":
     elif vars(args)["import"]:
         from src.UserImport import UserImporter
 
-        importer = UserImporter(file=args.user_file[0], config=config)
+        importer = UserImporter(input_file=args.user_file[0], config=config)
         importer.run()
