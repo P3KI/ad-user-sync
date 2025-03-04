452142dffd7e2e9a265f5eecc4618de50bb4e2d9
Philipp Claves <philipp@p3ki.com>
2025-03-04 11:20:19 +0000

    test: More test cases and support for multi-stage tests.

--- test/test.py
+++ test/test.py
@@ -4,29 +4,68 @@ import pathlib
 import os
 import json
 import shutil
+import sys
 from datetime import datetime
+import subprocess
 
 import dateutil.parser
 from dateutil.relativedelta import relativedelta
 
+from pyad import ADGroup, ADUser, win32Exception, ADContainer
+
+
 basePath = pathlib.Path(__file__).parent
 testPath = basePath / "cases"
 
+syncScript     = (basePath / ".." / "user_sync.py").absolute()
+importConfig   = (basePath / "import_config.json").absolute()
+readbackConfig = (basePath / "readback_config.json").absolute()
+
 
 def run_test(test):
-    print("Running test {}".format(test))
+    print("#### Running test {}".format(test))
     os.chdir(test)
-    os.remove("Resolutions.json")
-    shutil.copy("ResolutionsIn.json", "Resolutions.json")
-    os.system("python ../../../user_sync.py import --config ../../import_config.json")
-    os.system("python ../../../user_sync.py export --config ../../readback_config.json")
 
-    with open("Expected.json") as f:
-        expected_result = json.load(f)
-    with open("Readback.json") as f:
-        actual_result = json.load(f)
+    stages = list(test.glob("stage*"))
+    stages.sort()
+    if len(stages) == 0:
+        stages.append(None)
 
-    compare_lists(expected_result, actual_result)
+    with open(importConfig) as f:
+        config = json.load(f)
+
+    ret = True
+
+    for stage in stages:
+        if stage is not None:
+            print("  ## Stage '{}'".format(stage))
+            os.chdir(stage)
+
+        shutil.copy("ResolutionsIn.json", "Resolutions.json")
+        subprocess.run (["python", syncScript, "import", "--config", importConfig])
+        subprocess.run (["python", syncScript, "export", "--config", readbackConfig])
+        os.remove("Resolutions.json")
+
+
+        with open("Expected.json") as f:
+            expected_result = json.load(f)
+        with open("Readback.json") as f:
+            actual_result = json.load(f)
+
+
+        try:
+            compare_lists(expected_result, actual_result)
+        except AssertionError as e:
+            print("FAILURE: Test '{}' failed: {}".format(test, e), file=sys.stderr)
+            ret = False
+            break
+
+        os.remove("Readback.json")
+
+
+    cleanup(config)
+
+    return ret
 
 
 def compare_lists(expected, actual):
@@ -64,6 +103,33 @@ def sort_lists(l):
     return l.sort(key=lambda x: x['sAMAccountName'])
 
 
-for test in testPath.glob("*"):
-    run_test(test)
+def cleanup(config):
+    if len(config.get("managed_user_path", "")) > 0 :
+        managed_user_path = config["managed_user_path"] + "," +  config["base_path"]
+    else:
+        managed_user_path =  config["base_path"]
 
+    managed_user_container = ADContainer.from_dn(managed_user_path)
+    for user in managed_user_container.get_children_iter(True, [ADUser]):
+        #print("Cleanup user {}".format(user))
+        user.delete()
+
+
+count = 0
+failures = 0
+
+tests = list(testPath.glob("*"))
+tests.sort()
+
+for test in tests:
+    count += 1
+    if not run_test(test):
+        failures += 1
+
+
+if failures > 0:
+    print("Failed {} of {} tests".format(failures, count))
+    exit(1)
+else:
+    print("All {} tests passed!".format(count))
+    exit(0)
